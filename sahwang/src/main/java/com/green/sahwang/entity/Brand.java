6882c13daa980b674a1098864b958915b45aa7a6
songhochang <thdghckd111@naver.com>
2024-10-07 17:05:43 +0900

    [추가, 테스트] 기본세팅 + (멤버 + 구매 테스트)까지...

---
+++ sahwang/src/main/java/com/green/sahwang/entity/Brand.java
@@ -0,0 +1,4 @@
+package com.green.sahwang.entity;
+
+public class Brand {
+}
---
+++ sahwang/src/main/java/com/green/sahwang/entity/Member.java
@@ -0,0 +1,48 @@
+package com.green.sahwang.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "member")
+    private List<Purchase> purchases = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Purchase> getPurchases() {
+        return purchases;
+    }
+
+    public void setPurchases(List<Purchase> purchases) {
+        this.purchases = purchases;
+    }
+}
