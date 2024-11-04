9de75bd81078df38770cf69dae2a9a1f0dfb5a89
DESKTOP-SCJ83VA\user2 <qordi124@gmail.com>
2024-11-04 10:35:46 +0900

    [수정] init data

--- sahwang/app/src/main/resources/data.sql
+++ sahwang/app/src/main/resources/data.sql
@@ -26,6 +26,8 @@ INSERT INTO `member` (`member_id`, `account`, `ad_received`, `addr`, `post_code`
 VALUES (3, NULL, NULL, NULL, NULL, 'whgpals4263@nate.com', NULL, '2024-10-31', NULL, '민이♡', NULL, 'USER', 1, NULL, 'http://img1.kakaocdn.net/thumb/R640x640.q70/?fname=http://t1.kakaocdn.net/account_images/default_profile.jpeg', 'http://img1.kakaocdn.net/thumb/R110x110.q70/?fname=http://t1.kakaocdn.net/account_images/default_profile.jpeg');
 INSERT INTO `member` (`member_id`, `account`, `ad_received`, `addr`, `post_code`, `email`, `is_deleted`, `join_date`, `name`, `nick_name`, `phone_num`, `role`, `sns_type`, `withdraw_date`, `profile_image`, `thumbnail_image`)
 VALUES (4, NULL, NULL, NULL, NULL, 'kdh7313@naver.com', NULL, '2024-11-01', NULL, '강도현', NULL, 'USER', 1, NULL, 'http://k.kakaocdn.net/dn/b7yPFA/btsKquPi0Cw/BjABLmpyNB6gbKxUAX9I40/img_640x640.jpg', 'http://k.kakaocdn.net/dn/b7yPFA/btsKquPi0Cw/BjABLmpyNB6gbKxUAX9I40/img_110x110.jpg');
+INSERT INTO `member` (`member_id`, `account`, `ad_received`, `addr`, `post_code`, `email`, `is_deleted`, `join_date`, `name`, `nick_name`, `phone_num`, `role`, `sns_type`, `withdraw_date`, `profile_image`, `thumbnail_image`)
+VALUES (5, NULL, NULL, NULL, NULL, 'thdghckd111@naver.com', NULL, '2024-11-01', NULL, '송호창', NULL, 'USER', 1, NULL, 'http://img1.kakaocdn.net/thumb/R640x640.q70/?fname=http://t1.kakaocdn.net/account_images/default_profile.jpeg', 'http://img1.kakaocdn.net/thumb/R110x110.q70/?fname=http://t1.kakaocdn.net/account_images/default_profile.jpeg');
 
 insert into cart(cart_id, quantities, member_id)
 values (1, 3, 1);
